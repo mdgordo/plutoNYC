a6319ada7c2f30aacb2185575d5197edc236cfc1
mdgordo <mdgordo@gmail.com>
2017-11-16 20:50:59 -0500

    Update preprocess11.m

--- preprocess11.m
+++ preprocess11.m
@@ -151,7 +151,8 @@ bk11=[bk11 table(BuildingClass)];
 %% create value per square foot
 % special treatment of condos and vacant land? 
 
-ValPSF=bk11.AssessTot./bk11.BldgArea;
+
+ValPSF=(bk16.AssessTot-bk16.AssessLand)./bk16.BldgArea;
 ValPSF(isnan(ValPSF)==1)=0;
 ValPSF(isinf(ValPSF)==1)=0;
 bk11=[bk11 table(ValPSF)];
